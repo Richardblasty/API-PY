4cb7c7ae8bcd1be7969d1e912953d2a191b5712b
Richard Marques <richardblas31@gmail.com>
2026-03-24 23:45:56 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+# Create Database
+
+# Create Routes
+@app.route('/')
+def home():
+    return 'Hello, World!'
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
