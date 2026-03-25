21d66d2240f073631222aebc897c25ca2554b354
Richard legal <richardblas31@gmail.com>
2026-03-25 00:21:36 -0300

    Adição da página e de texto

--- main.py
+++ main.py
@@ -1,16 +1,42 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy #ORM
 
-app = Flask(__name__)
+app = Flask(__name__)   
 
 # Create Database
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///travel.db'
+
+db = SQLAlchemy(app)
+
+class Destination(db.Model): 
+    id = db.Column(db.Integer, primary_key=True)
+    destination = db.Column(db.String(50), nullable=False)
+    country = db.Column(db.String(50), nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'destination': self.destination,
+            'country': self.country,
+            'rating': self.rating
+        }  
+
+
+with app.app_context():
+    db.create_all()
+
 
 # Create Routes
 @app.route('/')
 def home():
-    return 'Hello, World!'
+    return jsonify({'message': 'Welcome to the Travel Destinations API!'})
 
 
+@app.route('/destinations',methods=['GET'])
+def get_destinations():
+    destinations = Destination.query.all()
+    return jsonify([destination.to_dict() for destination in destinations])
 
 
 if __name__ == '__main__':
